7d1bb77212bacbdb9bdef9927b64b694facc25a1
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2023-12-22 18:17:49 +0100

    Update admin_page.php

--- server/admin/admin_page.php
+++ server/admin/admin_page.php
@@ -6,5 +6,42 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin') {
     header("Location: ../index.php"); // Redirigir a la página de inicio de sesión
     exit();
 }
-echo("hola");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Administrador</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            position: relative;
+            padding-top: 50px; /* Ajusta según sea necesario */
+        }
+
+        .logout-btn {
+            position: absolute;
+            top: 10px; /* Ajusta según sea necesario */
+            right: 10px; /* Ajusta según sea necesario */
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+        <!-- Botón de Logout -->
+        <form class="logout-btn" action="../logout.php" method="post">
+            <input type="submit" value="Logout">
+        </form>
+
+        <h1>Bienvenido a la Página de Administrador</h1>
+
+        <!-- Contenido adicional de la página de administrador -->
+        <p>Este es el contenido de la página de administrador.</p>
+    </div>
+</body>
+</html>
