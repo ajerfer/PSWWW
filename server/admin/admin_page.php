34f3b8972883a960ff9a57da06acdad9ef945956
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2023-12-22 18:07:45 +0100

    Add files via upload

---
+++ server/admin/admin_page.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+// Verificar si el usuario ha iniciado sesión y es un administrador
+if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin') {
+    header("Location: ../index.php"); // Redirigir a la página de inicio de sesión
+    exit();
+}
+echo("hola");
+?>
