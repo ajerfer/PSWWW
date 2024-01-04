76b1c4177428d7d564a062dbb6b7a67fabb06f22
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-04 14:41:35 +0100

    Update manage_store.php

--- server/admin/manage_store.php
+++ server/admin/manage_store.php
@@ -1,3 +1,9 @@
 <?php
-echo("holaaa")
+session_start();
+
+// Verificar si el usuario ha iniciado sesión y es un administrador
+if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin') {
+    header("Location: ../index.php"); // Redirigir a la página de inicio de sesión
+    exit();
+}
 ?>
