4e11aac4b7336cac85696486e1e5019385913d6a
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-04 20:52:12 +0100

    Update index.php

--- server/index.php
+++ server/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Página Web</title>
-    <link rel="stylesheet" type="text/css" href="public/styles.css">
+    <link rel="stylesheet" type="text/css" href="../public/styles.css">
 </head>
 <body>
     <header>
