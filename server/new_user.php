236861cc5c7ab1f975e56c2f7fbaeaa901801575
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-04 14:39:21 +0100

    Add files via upload

---
+++ server/new_user.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creacion de Usuario</title>
+    <link rel="stylesheet" type="text/css" href="public/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
