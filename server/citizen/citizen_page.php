2ce0ba44f878f79a344d83e760ddd6a94866910a
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-20 17:05:50 +0100

    Update citizen_page.php

--- server/citizen/citizen_page.php
+++ server/citizen/citizen_page.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Verificar si el usuario ha iniciado sesión y es un administrador
-if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'citizen') {
+if (!isset($_SESSION['userId']) || $_SESSION['role'] !== 'citizen') {
     header("Location: ../index.php"); // Redirigir a la página de inicio de sesión
     exit();
 }
