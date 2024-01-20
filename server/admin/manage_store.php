f5ca93d4eec67dd62fc4ad33e3770491ec89325d
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-20 17:02:32 +0100

    Update manage_store.php

--- server/admin/manage_store.php
+++ server/admin/manage_store.php
@@ -1,9 +1,43 @@
 <?php
 session_start();
+include_once '../mongodbconnect.php';
 
 // Verificar si el usuario ha iniciado sesión y es un administrador
-if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin') {
+if (!isset($_SESSION['userId']) || $_SESSION['role'] !== 'admin') {
     header("Location: ../index.php"); // Redirigir a la página de inicio de sesión
     exit();
 }
+
+// Seleccionar la colección de productos
+$productsCollection = $dataBase->Products;
+
+// Recuperar todos los documentos de la colección
+$productos = $productsCollection->find();
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos</title>
+</head>
+<body>
+    <h1>Lista de Productos</h1>
+    <?php foreach ($productos as $producto): ?>
+        <div>
+            <ul>
+                <?php foreach ($producto['items'] as $item): ?>
+                    <li><?= $item['name']; ?></li>
+                    <ul>
+                        <?php foreach ($item['details'] as $detail): ?>
+                                <li><?= $detail['detail_name'] . ': ' . $detail['detail_value']; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endforeach; ?>
+</body>
+</html>
+
