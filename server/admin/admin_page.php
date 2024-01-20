260cad38bde3de4a4490c966bbc6df43f99a9ad9
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-20 17:02:05 +0100

    Update admin_page.php

--- server/admin/admin_page.php
+++ server/admin/admin_page.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Verificar si el usuario ha iniciado sesión y es un administrador
-if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'admin') {
+if (!isset($_SESSION['userId']) || $_SESSION['role'] !== 'admin') {
     header("Location: ../index.php"); // Redirigir a la página de inicio de sesión
     exit();
 }
