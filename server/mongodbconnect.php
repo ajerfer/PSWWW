d64955918d513953021cec2949153c8b32668f98
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-20 16:53:52 +0100

    Add files via upload

---
+++ server/mongodbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+// Datos de conexión a MongoDB
+$uri = "mongodb://localhost:27017";
+$dbName = "Web";
+
+try {
+    // Conectar a MongoDB
+    $client = new MongoDB\Client($uri);
+
+    // Seleccionar la base de datos
+    $dataBase = $client->$dbName;
+
+    echo "Conexión exitosa a la base de datos: $dbName";
+} catch (MongoDB\Exception\Exception $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
+
+?>
