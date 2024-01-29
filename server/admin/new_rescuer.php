67e6317461868dba063314922b1512e9e0c64a11
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-29 17:06:29 +0100

    Add files via upload

---
+++ server/admin/new_rescuer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Rescatador</title>
+    <link rel="stylesheet" type="text/css" href="../public/styles.css">
+    <style>
+        form {
+            display: flex;
+            flex-direction: column;
+            max-width: 300px;
+            margin: auto;
+        }
+
+        label {
+            margin-bottom: 5px;
+        }
+
+        input {
+            margin-bottom: 10px;
+            padding: 5px;
+        }
+
+        input[type="submit"] {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+
+    <h2>Registro de Rescuer</h2>
+
+    <form action="register_rescuer.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required>
+        <br><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required>
+        <br><br>
+        <label for="name">Name:</label>
+        <input type="text" name="name" required>
+        <br><br>
+        <label for="surname">Surname:</label>
+        <input type="text" name="surname" required>
+        <br><br>
+        <input type="submit" value="Register">
+    </form>
+
+</body>
+</html>
