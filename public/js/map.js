c7df641bfa097b4a4d9dd22ca1372398e43d9139
aaronjeronimo <ajeronimofernandez@gmail.com>
2024-01-29 13:40:02 +0200

    map change

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,18 @@
-var map = L.map('map').setView([38.246639, 21.74], 15);
-
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
-}
-
-map.on('click', onMapClick)
+var map = L.map('map', { zoomControl: false, dragging: false, scrollWheelZoom: false, doubleClickZoom: false}).setView([38.246639, 21.74], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    marker = new L.marker(e.latlng, {draggable:'true', color: 'red'});
+    marker.on('dragend', function(event){
+      var marker = event.target;
+      var position = marker.getLatLng();
+      marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
+    });
+    map.addLayer(marker);
+  };
+  
+  map.on('click', onMapClick);
---
+++ server/announcements.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Announcements</title>
+</head>
+<body>
+    <h1>Announcements</h1>
+    <button type="submit">Create</button>
+</body>
+</html>
