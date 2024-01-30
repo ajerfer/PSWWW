f9c808cf02eeb3f94bc5c8620104e33d81c25261
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-01-30 16:03:44 +0100

    Update save_new_product.php

--- server/admin/save_new_product.php
+++ server/admin/save_new_product.php
@@ -12,20 +12,16 @@ if (!isset($_SESSION['userId']) || $_SESSION['role'] !== 'admin') {
 $productsCollection = $dataBase->Products;
 
 // Obtener datos del formulario
-$newName = $_POST['itemName'];
-$newCategory = $_POST['category'];
-$newDetails = $_POST['detailName'];
-$newValues = $_POST['detailValue'];
+$newName = isset($_POST['itemName']) ? $_POST['itemName'] : '';
+$newCategory = isset($_POST['category']) ? $_POST['category'] : '';
+$newDetails = isset($_POST['detailName']) ? $_POST['detailName'] : [];
+$newValues = isset($_POST['detailValue']) ? $_POST['detailValue'] : [];
 
 $newProduct = [
-    'items' => [
-        [
-            'id' => uniqid(),
-            'name' => $newName,
-            'category' => $newCategory,
-            'details' => []
-        ]
-    ]
+    'id' => uniqid(),
+    'name' => $newName,
+    'category' => $newCategory,
+    'details' => []
 ];
 
 if (!empty($newDetails) && !empty($newValues)) {
@@ -34,15 +30,20 @@ if (!empty($newDetails) && !empty($newValues)) {
         $detailName = $newDetails[$i];
         $detailValue = $newValues[$i];
 
-        $newProduct['items'][0]['details'][] = [
+        $newProduct['details'][] = [
             'detail_name' => $detailName,
             'detail_value' => $detailValue
         ];
     }
 }
-$result = $productsCollection->insertOne($newProduct);
+$filter = ['_id' => new MongoDB\BSON\ObjectId('65ae85d89abdeb149ebd2d68')];
+$result = $productsCollection->updateOne(
+    $filter,
+    ['$push' => ['items' => $newProduct]]
+);
+//$result = $productsCollection->insertOne($newProduct);
 
-if ($result->getInsertedCount() > 0) {
+if ($result->getModifiedCount() > 0) {
     echo "Producto insertado correctamente.";
 } else {
     echo "Error al insertar el producto.";
