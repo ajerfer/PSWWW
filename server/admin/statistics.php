152ff80012ad9e5cd2527d45314b3cb83bd05d35
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-02-08 14:08:43 +0100

    Update statistics.php

--- server/admin/statistics.php
+++ server/admin/statistics.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['userId']) || $_SESSION['role'] !== 'admin') {
+    header("Location: ../index.php"); 
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
