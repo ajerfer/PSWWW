2dd252079b380f735cab943404b234f39db935c6
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-02-08 19:31:06 +0100

    Update filter_categories.php

--- server/admin/filter_categories.php
+++ server/admin/filter_categories.php
@@ -7,7 +7,7 @@ $uniqueCategories = $productsCollection->distinct('categories', [], ['id', 'cate
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
