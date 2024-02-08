120ece8d2c3524b50eea2a41526d554fc4928909
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-02-08 19:27:36 +0100

    Update register_user.php

--- server/register_user.php
+++ server/register_user.php
@@ -4,9 +4,9 @@ session_start();
 include_once "databaseconnect.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recuperar datos del formulario
+
     $username = $_POST["username"];
-    $password = $_POST["password"]; // Hash de la contraseña
+    $password = $_POST["password"];
     $name = $_POST["name"];
     $surname = $_POST["surname"];
     $phone = $_POST["phone"];
@@ -15,34 +15,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $con = connect();
 
-    // Verificar la conexión
     if ($con->connect_error) {
         die("Fatal connection: " . $con->connect_error);
     }
 
-    // Insertar datos en la base de datos
     $queryUser = "INSERT INTO Users (username, password, role) 
               VALUES ('$username', '$password', 'citizen')";
     mysqli_query($con, $queryUser);
 
-    // Obtener el ID del usuario recién insertado
     $userId = mysqli_insert_id($con);
-
-    // Insertar datos en la tabla Citizen
 
     $queryCitizen = "INSERT INTO Citizens (userId, name, surname, phone, lat, lng)
                  VALUES ('$userId', '$name', '$surname', '$phone', '$lat', '$lng')";
     mysqli_query($con, $queryCitizen);
 
-    // Verificar si la inserción fue exitosa
     if(mysqli_affected_rows($con) > 0) {
-        // Redirigir al usuario de vuelta a la página de inicio de sesión
         header("Location: index.php");
         exit();
     } else {
-        echo "Error en la inserción: " . mysqli_error($con);
+        echo "Insertion error: " . mysqli_error($con);
     }
-    // Cerrar la conexión a la base de datos
     $conn->close();
 } else {
     // Redirigir si se accede directamente a este script sin enviar datos por POST
