4b1331165da130026e4d2833a01304e13d79b2de
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-02-08 19:23:45 +0100

    Update login.php

--- server/login.php
+++ server/login.php
@@ -3,25 +3,21 @@ session_start();
 
 include_once "databaseconnect.php";
 
-// Obtener datos del formulario
 $username = $_POST['username'];
 $password = $_POST['password'];
 $con = connect();
-// Consultar la base de datos para verificar las credenciales
+
 $query = "SELECT * FROM Users WHERE username = '$username' AND password = '$password'";
 $result = mysqli_query($con, $query);
 
 if ($result) {
-    // Verificar si se encontró un usuario con las credenciales proporcionadas
     if (mysqli_num_rows($result) == 1) {
         $user = mysqli_fetch_assoc($result);
 
-        // Guardar información del usuario en la sesión
         $_SESSION['userId'] = $user['userId'];
         $_SESSION['username'] = $user['username'];
         $_SESSION['role'] = $user['role'];
 
-        // Redireccionar a la página correspondiente según el tipo de usuario
         switch ($_SESSION['role']) {
             case 'admin':
                 header("Location: admin/admin_page.php");
@@ -33,11 +29,10 @@ if ($result) {
                 header("Location: map.php");
                 break;
             default:
-                // Manejar cualquier otro tipo de usuario según sea necesario
                 break;
         }
     } else {
-        echo "Credenciales incorrectas. Vuelva a intentarlo.";
+        echo "Incorrect credentials. Try again.";
     }
 } else {
     echo "Error al realizar la consulta.";
