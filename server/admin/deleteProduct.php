8f28ef13263bd1f4c13612fa074883866ee2d6ef
mkhudzey <124878089+mkhudzey@users.noreply.github.com>
2024-02-08 19:30:37 +0100

    Update deleteProduct.php

--- server/admin/deleteProduct.php
+++ server/admin/deleteProduct.php
@@ -2,37 +2,28 @@
 session_start();
 include_once '../mongodbconnect.php';
 
-// Verificar si el usuario ha iniciado sesión y es un administrador
 if (!isset($_SESSION['userId']) || $_SESSION['role'] !== 'admin') {
-    header("Location: ../index.php"); // Redirigir a la página de inicio de sesión
+    header("Location: ../index.php");
     exit();
 }
 
-/// Seleccionar la colección de productos
 $productsCollection = $dataBase->Products;
 
-// Obtener datos del formulario de modificación
+
 $itemId = $_POST['itemId'];
 
-// Imprimir el valor para verificar que sea correcto
-var_dump($itemId);
-
-// Definir el filtro para eliminar el documento con el ID proporcionado
 $filter = ['items.id' => $itemId];
 
-// Definir la actualización utilizando $pull para eliminar el elemento del array
 $update = ['$pull' => ['items' => ['id' => $itemId]]];
 
-// Eliminar el documento de la colección
 $result = $productsCollection->updateOne($filter, $update);
 
 if ($result->getModifiedCount() > 0) {
-    echo "Producto eliminado correctamente.";
+    echo "Product deleted";
 } else {
-    echo "Error al eliminar el producto.";
+    echo "Error";
 }
 
-// Redir
 header("Location: manage_store.php");
 exit();
 ?>
